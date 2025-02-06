587983718700fbd71090e86f83dbda744c1428c3
Sudarshan Giri <sudarshanhang@gmail.com>
2025-02-06 20:51:51 +0545

    Add other images to the queue first

--- Drupal 10/modules/custom/collector_systems/src/Synchronizer.php
+++ Drupal 10/modules/custom/collector_systems/src/Synchronizer.php
@@ -35,24 +35,24 @@ class Synchronizer
       ]);
     }
 
-    //Add object Images data to the queue
-    $form_object_images = new \Drupal\collector_systems\Form\ObjectImagesImportForm();
-    $data_object_images = $form_object_images->getDataForProcessing();
-    // Add items to the queue.
-    $queue_type = 'object_images';
-    foreach ($data_object_images as $item) {
-      $queue->createItem([
-        'data' => $item,
-        'queue_type' => $queue_type
-      ]);
-    }
-
     //Add Other Images data to the queue
     $form_other_images = new \Drupal\collector_systems\Form\OtherImagesImportForm();
     $data_other_images = $form_other_images->getDataForProcessing();
     // Add items to the queue.
     $queue_type = 'other_images';
     foreach ($data_other_images as $item) {
+      $queue->createItem([
+        'data' => $item,
+        'queue_type' => $queue_type
+      ]);
+    }
+
+    //Add object Images data to the queue
+    $form_object_images = new \Drupal\collector_systems\Form\ObjectImagesImportForm();
+    $data_object_images = $form_object_images->getDataForProcessing();
+    // Add items to the queue.
+    $queue_type = 'object_images';
+    foreach ($data_object_images as $item) {
       $queue->createItem([
         'data' => $item,
         'queue_type' => $queue_type
