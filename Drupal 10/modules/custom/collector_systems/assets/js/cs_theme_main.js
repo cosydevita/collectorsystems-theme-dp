210bd310459bff1ac4c7ebcde3a72eafe912c4d2
Sudarshan Giri <sudarshanhang@gmail.com>
2025-08-18 19:32:00 +0545

    Replace bootstrap carousel with custom carousel to avoid conflicts with bootstrap libraries used by client websites

--- Drupal 10/modules/custom/collector_systems/assets/js/cs_theme_main.js
+++ Drupal 10/modules/custom/collector_systems/assets/js/cs_theme_main.js
@@ -68,110 +68,14 @@
     });
 
 
+    $(document).ready(function(){
+    // Add background color dynamically to the image wrapper link in the center aligned images.
+    let cs_bg_image_color = $('body').data('cs-image-bg');
 
-
-// var carouselA = document.getElementById('carouselA')
-// var carouselB = document.getElementById('carouselB')
-
-// if(carouselA != null)
-// {
-// carouselA.addEventListener('slide.bs.carousel', function(e) {
-//     var bsCarouselB = bootstrap.Carousel.getInstance(carouselB)
-//     bsCarouselB.to(e.to)
-
-// 	var id = parseInt($(e.relatedTarget).attr("data-slide-number"));
-
-// 	var thumbNum = parseInt(
-// 		$("[id=carousel-selector-" + id + "]")
-// 			.parent()
-// 			.attr("data-slide-number")
-// 	);
-// 	$("[id^=carousel-selector-]").removeClass("selected");
-// 	$("[id=carousel-selector-" + id + "]").addClass("selected");
-// 	$("#carouselB").carousel(thumbNum);
-// })
-// }
-
-// handles the carousel thumbnails
-
-$(document).on("click", ".thumb", function (ev) {
-
-    $('#carouselB').find('.thumb').removeClass('selected');
-    if($(this).addClass('selected').closest('div').length > 0)
-    {
-        var selectorIdx = $(this).addClass('selected').closest('div').index();
-        $('#carouselB').find('.thumb').removeClass('selected')
-            .eq(selectorIdx).addClass('selected');
-
-        $('#carouselA').carousel(selectorIdx);
+    if(cs_bg_image_color){
+        $('body.cs-center-align-images .card-body a.image-wrapper-link').css('background', cs_bg_image_color);
     }
-});
-
-$('.carousel-inner.thumb-carousel').each(function() {
-    if ($(this).children('div').length <= 4)
-    {
-        $('.prev-part').hide();
-        $('.next-part').hide();
-        $(this).parent().parent().css("background-color","transparent");
-    }
-    else{
-        $(this).parent().parent().css("background-color","rgb(240, 240, 240)");
-    }
-});
-
-//code
-let items = document.querySelectorAll('#carouselB .carousel-item')
-
-items.forEach((el) => {
-    const minPerSlide = items.length > 4 ? 4 : items.length;
-    let next = el.nextElementSibling
-
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
-let carouselAItems = document.querySelectorAll('#carouselA .carousel-item')
-
-const slideShowUrlsA =[];
-
-for(var k=0;k<carouselAItems.length;k++)
-{
-    if($(carouselAItems[k]).length > 0)
-    {
-        if($(carouselAItems[k]).find("a.carouselAToggle").length > 0)
-        {
-            slideShowUrlsA.push($(carouselAItems[k]).find("a.carouselAToggle").attr("data-big"));
-        }
-    }
-}
-
-// const { BootstrapLightbox } = BootstrapLightboxModule;
-// if($("a.carouselAToggle").length > 0)
-// {
-//     document.addEventListener('DOMContentLoaded', () => {
-//         const bootstrapLightboxA = new BootstrapLightbox('.carouselAToggle', {
-//             name: 'carouselAGallery',
-//             data: slideShowUrlsA,
-//             drag: true
-//         });
-//         bootstrapLightboxA.createGallery();
-//     });
-// }
-
-$(document).ready(function(){
-  // Add background color dynamically to the image wrapper link in the center aligned images.
-  let cs_bg_image_color = $('body').data('cs-image-bg');
-
-  if(cs_bg_image_color){
-    $('body.cs-center-align-images .card-body a.image-wrapper-link').css('background', cs_bg_image_color);
-}
-})
+    })
 
 
 })(jQuery);
